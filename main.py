91c47526645fa0e48c6134ae8189c43003fde302
Shilpa Dhagat <shilpa.dhagat15@gmail.com>
2020-02-09 12:28:06 -0500

    initial commit sample hello world

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+# [START functions_helloworld_pubsub]
+def hello_pubsub(event, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
+    Args:
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata. The `event_id` field contains the Pub/Sub message ID. The
+         `timestamp` field contains the publish time.
+    """
+    import base64
+
+    print("""This Function was triggered by messageId {} published at {}
+    """.format(context.event_id, context.timestamp))
+
+    name = 'World'
+    print('Hello {}!'.format(name))
+# [END functions_helloworld_pubsub]
