b519687b0a19481f1e2b2e538a39d0a65d368e06
Shilpa Dhagat <shilpa.dhagat15@gmail.com>
2020-02-09 21:51:34 -0500

    scrape earnings data and test cloud sql connection

--- main.py
+++ main.py
@@ -1,4 +1,57 @@
-import sys
+import sys, requests
+import requests
+from bs4 import BeautifulSoup
+#import sqlalchemy
+import os
+
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+db_name = os.environ.get("DB_NAME")
+cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
+
+# [START cloud_sql_mysql_sqlalchemy_create]
+# The SQLAlchemy engine will help manage interactions, including automatically
+# managing a pool of connections to your database
+db = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+    sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        username=db_user,
+        password=db_pass,
+        database=db_name,
+        query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
+    ),
+    # ... Specify additional properties here.
+    # [START_EXCLUDE]
+    # [START cloud_sql_mysql_sqlalchemy_limit]
+    # Pool size is the maximum number of permanent connections to keep.
+    pool_size=5,
+    # Temporarily exceeds the set pool_size if no connections are available.
+    max_overflow=2,
+    # The total number of concurrent connections for your application will be
+    # a total of pool_size and max_overflow.
+    # [END cloud_sql_mysql_sqlalchemy_limit]
+    # [START cloud_sql_mysql_sqlalchemy_backoff]
+    # SQLAlchemy automatically uses delays between failed connection attempts,
+    # but provides no arguments for configuration.
+    # [END cloud_sql_mysql_sqlalchemy_backoff]
+    # [START cloud_sql_mysql_sqlalchemy_timeout]
+    # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+    # new connection from the pool. After the specified amount of time, an
+    # exception will be thrown.
+    pool_timeout=30,  # 30 seconds
+    # [END cloud_sql_mysql_sqlalchemy_timeout]
+    # [START cloud_sql_mysql_sqlalchemy_lifetime]
+    # 'pool_recycle' is the maximum number of seconds a connection can persist.
+    # Connections that live longer than the specified amount of time will be
+    # reestablished
+    pool_recycle=1800,  # 30 minutes
+    # [END cloud_sql_mysql_sqlalchemy_lifetime]
+    # [END_EXCLUDE]
+)
+# [END cloud_sql_mysql_sqlalchemy_create]
+
 
 # [START functions_helloworld_pubsub]
 def hello_pubsub(event, context):
@@ -11,11 +64,57 @@ def hello_pubsub(event, context):
          metadata. The `event_id` field contains the Pub/Sub message ID. The
          `timestamp` field contains the publish time.
     """
-    import base64
 
-    print("""This Function was triggered by messageId {} published at {}
-    """.format(context.event_id, context.timestamp))
+    earnings_date_scraper()
 
     name = 'World'
-    print('Hello {}!'.format(name))
+    print('Hello {}!'.format(name)) 
 # [END functions_helloworld_pubsub]
+
+def earnings_date_scraper():
+    data = []
+    url = "https://finance.yahoo.com/calendar/earnings?from=2020-02-09&to=2020-02-15&day=2020-02-09"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id='cal-res-table')
+    
+    table = results.find('table', class_="W(100%)")
+    
+    table_body = table.find('tbody')
+
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])
+    print(data)
+    save_earnings_data(data)
+
+def save_earnings_data(data):
+    stmt = sqlalchemy.text(
+            "INSERT INTO company (name, ticker)" " VALUES ('abc', 'xyz')"
+        )
+        try:
+            # Using a with statement ensures that the connection is always released
+            # back into the pool at the end of statement (even if an error occurs)
+            with db.connect() as conn:
+                conn.execute(stmt, time_cast=time_cast, candidate=team)
+        except Exception as e:
+            # If something goes wrong, handle the error in this section. This might
+            # involve retrying or adjusting parameters depending on the situation.
+            # [START_EXCLUDE]
+            logger.exception(e)
+            return Response(
+                status=500,
+                response="Unable to successfully cast vote! Please check the "
+                "application logs for more details.",
+            )
+            # [END_EXCLUDE]
+        # [END cloud_sql_mysql_sqlalchemy_connection]
+
+        return Response(
+            status=200,
+            response="Vote successfully cast for '{}' at time {}!".format(team, time_cast),
+        )
+
+
