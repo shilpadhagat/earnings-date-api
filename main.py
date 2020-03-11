4a0d86080411de655ada21de619d086e5babf18b
Shilpa Dhagat <shilpa.dhagat15@gmail.com>
2020-03-11 19:35:28 -0400

    test mysql connection

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
 import sys, requests
-import requests
 from bs4 import BeautifulSoup
-#import sqlalchemy
 import os
+import sqlalchemy
+import pymysql
+from pymysql.err import OperationalError
 
-db_user = os.environ.get("DB_USER")
-db_pass = os.environ.get("DB_PASS")
-db_name = os.environ.get("DB_NAME")
-cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
+CONNECTION_NAME = 'poised-lens-267620:us-east1:personalproject1'
+DB_HOST = '35.237.194.3'
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASS")
+DB_NAME = os.environ.get("DB_NAME")
 
-# [START cloud_sql_mysql_sqlalchemy_create]
-# The SQLAlchemy engine will help manage interactions, including automatically
-# managing a pool of connections to your database
-db = sqlalchemy.create_engine(
-    # Equivalent URL:
-    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
-    sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=db_user,
-        password=db_pass,
-        database=db_name,
-        query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
-    ),
-    # ... Specify additional properties here.
-    # [START_EXCLUDE]
-    # [START cloud_sql_mysql_sqlalchemy_limit]
-    # Pool size is the maximum number of permanent connections to keep.
-    pool_size=5,
-    # Temporarily exceeds the set pool_size if no connections are available.
-    max_overflow=2,
-    # The total number of concurrent connections for your application will be
-    # a total of pool_size and max_overflow.
-    # [END cloud_sql_mysql_sqlalchemy_limit]
-    # [START cloud_sql_mysql_sqlalchemy_backoff]
-    # SQLAlchemy automatically uses delays between failed connection attempts,
-    # but provides no arguments for configuration.
-    # [END cloud_sql_mysql_sqlalchemy_backoff]
-    # [START cloud_sql_mysql_sqlalchemy_timeout]
-    # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-    # new connection from the pool. After the specified amount of time, an
-    # exception will be thrown.
-    pool_timeout=30,  # 30 seconds
-    # [END cloud_sql_mysql_sqlalchemy_timeout]
-    # [START cloud_sql_mysql_sqlalchemy_lifetime]
-    # 'pool_recycle' is the maximum number of seconds a connection can persist.
-    # Connections that live longer than the specified amount of time will be
-    # reestablished
-    pool_recycle=1800,  # 30 minutes
-    # [END cloud_sql_mysql_sqlalchemy_lifetime]
-    # [END_EXCLUDE]
-)
-# [END cloud_sql_mysql_sqlalchemy_create]
+mysql_config = {
+    'host': DB_HOST,
+    'user': DB_USER,
+    'password': DB_PASSWORD,
+    'db': DB_NAME,
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor,
+    'autocommit': True
+}
 
+mysql_conn = None
+
+def __get_cursor():
+    """
+    Helper function to get a cursor
+      PyMySQL does NOT automatically reconnect,
+      so we must reconnect explicitly using ping()
+    """
+    try:
+        return mysql_conn.cursor()
+    except OperationalError:
+        mysql_conn.ping(reconnect=True)
+        return mysql_conn.cursor()
 
 # [START functions_helloworld_pubsub]
-def hello_pubsub(event, context):
+def hello_pubsub():
     """Background Cloud Function to be triggered by Pub/Sub.
     Args:
          event (dict):  The dictionary with data specific to this type of
@@ -64,11 +46,8 @@ def hello_pubsub(event, context):
          metadata. The `event_id` field contains the Pub/Sub message ID. The
          `timestamp` field contains the publish time.
     """
-
     earnings_date_scraper()
 
-    name = 'World'
-    print('Hello {}!'.format(name)) 
 # [END functions_helloworld_pubsub]
 
 def earnings_date_scraper():
@@ -91,30 +70,32 @@ def earnings_date_scraper():
     save_earnings_data(data)
 
 def save_earnings_data(data):
-    stmt = sqlalchemy.text(
-            "INSERT INTO company (name, ticker)" " VALUES ('abc', 'xyz')"
-        )
+    ensure_mysql_conn()
+
+    sql_insert_query = """INSERT INTO `company`
+        (`name`, `ticker`)
+    VALUES
+        ('abc', 'xyz')
+    ON DUPLICATE KEY UPDATE
+        `actual` = VALUES(actual)"""
+
+    with __get_cursor() as cursor:
         try:
-            # Using a with statement ensures that the connection is always released
-            # back into the pool at the end of statement (even if an error occurs)
-            with db.connect() as conn:
-                conn.execute(stmt, time_cast=time_cast, candidate=team)
-        except Exception as e:
-            # If something goes wrong, handle the error in this section. This might
-            # involve retrying or adjusting parameters depending on the situation.
-            # [START_EXCLUDE]
-            logger.exception(e)
-            return Response(
-                status=500,
-                response="Unable to successfully cast vote! Please check the "
-                "application logs for more details.",
-            )
-            # [END_EXCLUDE]
-        # [END cloud_sql_mysql_sqlalchemy_connection]
+            result = cursor.executemany(sql_insert_query)
+        except Exception as exc:
+            raise RuntimeError("company: {}".format(company)) from exc
+        print (cursor.rowcount, "Record inserted successfully into actuals table")
 
-        return Response(
-            status=200,
-            response="Vote successfully cast for '{}' at time {}!".format(team, time_cast),
-        )
+def ensure_mysql_conn():
+    global mysql_conn
 
+    if not mysql_conn:
+        try:
+            mysql_conn = pymysql.connect(**mysql_config)
+        except OperationalError:
+            # If production settings fail, use local development ones
+            mysql_config['unix_socket'] = f'/cloudsql/{CONNECTION_NAME}'
+            mysql_conn = pymysql.connect(**mysql_config)
+
+hello_pubsub()
 
